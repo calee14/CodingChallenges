84261f2e3c2298307eb880b746e39040d4e0b7ba
calee14 <caplee1000@gmail.com>
2018-11-03 20:55:47 -0700

    added answer

--- challenge_1/find_sum_in_list.py
+++ challenge_1/find_sum_in_list.py
@@ -41,3 +41,31 @@ if end_time_1 > end_time_2:
   print('Mine is faster')
 else:
   print('O(N^2) is faster')
+
+# Answer
+def find_sum_(list_nums, target):
+    '''
+    Use a hash set to store the number we have seen. Then whenever we see a new
+    number, check whether the difference of it and the target already in the
+    hash set.
+    Args:
+        list_nums(list): a list of number
+        target(int): the sum that is looked for
+    returns:
+        bool: whether there is a pair of numbers add up to target
+    '''
+    num_seen_set = set()
+    for num in list_nums:
+      print(num_seen_set)
+      print(target - num)
+      if target - num in num_seen_set:
+          return True
+      else:
+          num_seen_set.add(num)
+    return False
+
+start_time_3 = time.time()
+result_3 = find_sum_(numbers, 17)
+end_time_3 = time.time() - start_time_3
+print(end_time_3, "s")
+print(result_3)
