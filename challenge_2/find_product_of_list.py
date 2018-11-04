efbf7cec59f2f172b94439883500ef48df4f4686
calee14 <caplee1000@gmail.com>
2018-11-03 21:02:19 -0700

    added answer

--- challenge_2/find_product_of_list.py
+++ challenge_2/find_product_of_list.py
@@ -31,3 +31,35 @@ def answer_without_division(nums):
   return result
 answer = answer_without_division(num_list)
 print(answer)
+
+# Answer
+def solution(list_nums):
+  '''
+  Without using division, have to create a (k,v) dictionary to store the index
+  and the multiplication result to the left of each num we see in first scan.
+  Then create the new list by go from right to left in second run. At the same
+  time, the multiplication to the right need to be stored.
+  Args:
+      list_nums(list): a list of numbers
+  Returns:
+      list: a new list
+  '''
+  mul_left = dict()
+  mul_right = dict()
+
+  new_list = [None] * len(list_nums)  # best practice: use None to initiate
+
+  mul_left[-1] = 1
+  for i in range(len(list_nums) - 1):
+      mul_left[i] = list_nums[i] * mul_left[i - 1]
+
+  mul_right[len(list_nums)] = 1
+  for j in reversed(range(len(list_nums))[1:]):
+    mul_right[j] = list_nums[j] * mul_right[j + 1]
+
+  for i in range(len(list_nums)):
+    new_list[i] = mul_left[i - 1] * mul_right[i + 1]
+
+  return new_list
+answer = solution(num_list)
+print(answer)
