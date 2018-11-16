a95dff5a7f80a671c0d3033247b76f95a2408471
calee14 <caplee1000@gmail.com>
2018-11-15 23:20:17 -0800

    added correct solution for #14

--- challenge_14/pi.py
+++ challenge_14/pi.py
@@ -8,3 +8,35 @@ Hint: The basic equation of a circle is x2 + y2 = r2.
 
 # The math concepts for this problem are too complex for me, so I'll just skip to the answer
 # The correct solution
+import random
+import math
+
+random.seed(0xBADC0FFE)
+
+def est_pi(num):
+    cnt = 0
+    total = 0
+    prev_pi, pi_approx = 0, 3
+
+    while True:
+        point = list()
+        for i in range(num):
+            point.append((random.random(), random.random()))
+            if point[i][0] ** 2 + point[i][1] ** 2 <= 1:
+                cnt += 1
+            total += 1
+        prev_pi = pi_approx
+        pi_approx = 4 * cnt / float(total)
+        print 'prev_pi = {}, pi_approx = {}'.format(prev_pi, pi_approx)
+        if abs(prev_pi - pi_approx) < 1e-9:    # assume we don't know math.pi
+            return pi_approx
+        # if abs(math.pi - pi_approx) < 1e-4:   # assume math.pi is known
+        #     return pi_approx
+
+
+def main():
+    print est_pi(1000)
+
+
+if __name__ == '__main__':
+    main()
