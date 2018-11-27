914298a077996cce89defe02866482a2b4a88f42
calee14 <caplee1000@gmail.com>
2018-11-26 17:44:07 -0800

    commenting code and now understand

--- challenge_15/select_random_from_memory.py
+++ challenge_15/select_random_from_memory.py
@@ -16,16 +16,22 @@ import random
 random.seed(0xBADC0FFE)
 
 def sample_gen_func(num):
+    # creates a generator that's too large to store into memory
+    # the generator store all numbers in 1000
     for x in xrange(num):
         yield x
 
 def solution(sample_generator):
+    # used to store the index 'i'
     sample_count = 0
     selected_sample = None
 
     for sample in sample_generator:
+        # update the index of the sample
         sample_count += 1
+        # if the random number is less than the probability 1/i
         if random.random() <= 1.0 / sample_count:
+            # update the randomly selected sample
             selected_sample = sample
 
     return selected_sample
