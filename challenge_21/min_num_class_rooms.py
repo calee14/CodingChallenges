f76333b9a29572efd264528fc1f1d9a59e87a510
calee14 <caplee1000@gmail.com>
2018-12-02 17:59:19 -0800

    understanding code

--- challenge_21/min_num_class_rooms.py
+++ challenge_21/min_num_class_rooms.py
@@ -23,21 +23,22 @@ def find_min_rooms(time_list):
     Returns:
         int
     '''
-    start = sorted([num[0] for num in time_list])
-    end = sorted([num[1] for num in time_list])
+    start = sorted([num[0] for num in time_list]) # organize all the start times in one list
+    end = sorted([num[1] for num in time_list]) # organize all the end times in one list
 
+    # initialize variables
     room_occupied = max_room_num = 0
     i = j = 0
     n = len(time_list)
 
     while i < n and j < n:
-        if start[i] < end[j]:
+        if start[i] < end[j]: # if the class starts before the last class ended
             room_occupied += 1
-            max_room_num = max(room_occupied, max_room_num)
-            i += 1
+            max_room_num = max(room_occupied, max_room_num) # find the new max room number
+            i += 1 # move to the start time of the next lecture
         else:
             room_occupied -= 1
-            j += 1
+            j += 1 # move to the end time of the next class
 
     return max_room_num
 
@@ -45,7 +46,7 @@ def find_min_rooms(time_list):
 def main():
     time_list = [(30, 75), (0, 50), (60, 150)]
     time_list2 = [(90,91), (94, 120), (95, 112), (110, 113), (150, 190), (180, 200)]
-    print find_min_rooms(time_list2)
+    print(find_min_rooms(time_list2))
 
 if __name__ == '__main__':
     main()
